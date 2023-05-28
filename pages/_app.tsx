5c9a23966bd9afd54e1f5197e6682818e81ef56d
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-05-28 02:02:33 -0500

    adding vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout, PageTransition } from "../components";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <PageTransition>
         <Component {...pageProps} />
+        <Analytics />
       </PageTransition>
     </Layout>
   );
